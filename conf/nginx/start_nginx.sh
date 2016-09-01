1b6fb09fdbcd06c8d4d0fc5578ffd3e59fbee364
Nikolay Denev <ndenev@debian.home.lan>
2016-09-01 08:39:55 +0200

    fixes

--- conf/nginx/start_nginx.sh
+++ conf/nginx/start_nginx.sh
@@ -8,8 +8,11 @@ docker-gen -notify 'killall -HUP nginx' -watch -only-exposed \
  -endpoint unix:///tmp/docker.sock \
  /etc/nginx/nginx.conf.tmpl /etc/nginx/nginx.conf &
 
-# Wait for config to be generated for the first time.
-inotifywait -t2 -e create,modify --exclude docker-gen /etc/nginx
+# Wait for config to be generated for the first time if it's not there yet
+if [ ! -e /etc/nginx/nginx.conf ]; then
+    echo Waiting 5 secs for /etc/nginx/nginx.conf to be generated for the first time.
+    inotifywait -t5 -e create,modify --exclude docker-gen /etc/nginx
+fi
 
 # Start nginx
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
